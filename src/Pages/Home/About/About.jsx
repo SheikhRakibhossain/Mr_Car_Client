3f5438b4c2448db01b869353e887f45591e655ff
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-15 01:38:39 +0600

    hello

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -13,8 +13,8 @@ const About = () => {
                         <h1 className="text-xl font-bold text-orange-500">About Us</h1>
                         <h1 className="text-5xl font-bold capitalize leading-[4rem] tracking-wide">We are qualified & of experience in this field</h1>
                         <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don{"'"}t look even slightly believable. </p>
-                        <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don{"'"}t look even slightly believable. </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <p className="py-6">We are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don{"'"}t look even slightly believable. </p>
+                        <button className="btn btn-primary">Get Started with us !!!</button>
                     </div>
                 </div>
             </div>
